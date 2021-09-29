922daa36a27598f4b146e01f9d3cd1e0a453d993
adbenitez <asieldbenitez@gmail.com>
2021-09-29 01:06:53 -0400

    generate version from git tags

--- simplebot_score/__init__.py
+++ simplebot_score/__init__.py
@@ -2,13 +2,18 @@ import os
 
 import simplebot
 from deltachat import Message
+from pkg_resources import DistributionNotFound, get_distribution
 from simplebot import DeltaBot
 from simplebot.bot import Replies
 from sqlalchemy import func
 
 from .orm import User, init, session_scope
 
-__version__ = "1.0.0"
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "0.0.0.dev0-unknown"
 
 
 @simplebot.hookimpl
